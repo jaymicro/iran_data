7759fd4dad30053aba0770459eab7fbc4b3be8ac
jaymicro <jay.envs@gmail.com>
2021-01-22 15:11:09 -0800

    function to change colnames of multiple dataframe

---
+++ Function_to_change_colnames_of_multiple_dfs.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+change_names <- function(x){
+  nw_name <- str_extract(colnames(x), "^[a-z]+_[a-z]+")
+  names(x) <- nw_name
+  return(x)
+}
+
+df <- list(df_percent_cover_kho_GA1 ,
+       df_percent_cover_kho_GA2 ,
+       df_percent_cover_kho_GA3 ,
+       df_percent_cover_kho_GA4 , 
+       df_percent_cover_masouleh_EA1 ,
+       df_percent_cover_maz_java_GA1,  
+       df_percent_cover_masouleh_GA1 ,
+       df_percent_cover_masouleh_GA2 ,
+       df_percent_cover_maz_java_EA1 ,
+       df_percent_cover_maz_java_GA1 ,
+       df_percent_cover_maz_java_GA2 ,
+       df_percent_cover_maz_po_EA1 ,
+       df_percent_cover_maz_po_GA1 ,
+       df_percent_cover_maz_po_GA2 ,
+       df_percent_cover_ramian_GA1 ,
+       df_percent_cover_ramian_GA2 ,
+       df_percent_cover_masouleh_EA1)
+
+nw_df <- lapply(df, change_names)  
+
