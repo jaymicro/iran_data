457565c0d05a126208a809d0fe5506971fc70185
Laura Ploughe <ploughel@gmail.com>
2021-02-18 12:13:07 -0800

    data wrangle for combining percent cover and biomass

--- Function_to_change_colnames_of_multiple_dfs.R
+++ Function_to_change_colnames_of_multiple_dfs.R
@@ -32,4 +32,32 @@ combined_df_percent_cover_nw <-
   select(sort(tidyselect::peek_vars()))%>%
   select(-column_label)
 
+write.csv(combined_df_percent_cover_nw, file ="percentcover_correct.csv" )
 
+df.bio <- list(df_biomass_kho_GA1 ,
+           df_biomass_kho_GA2 ,
+           df_biomass_kho_GA3 ,
+           df_biomass_kho_GA4 , 
+           df_biomass_masouleh_EA1 ,
+           df_biomass_masouleh_EA2,
+           df_biomass_masouleh_GA1 ,
+           df_biomass_masouleh_GA2 ,
+           df_biomass_maz_java_EA1 ,
+           df_biomass_maz_java_GA1,  
+           df_biomass_maz_java_GA2 ,
+           df_biomass_maz_po_EA1 ,
+           df_biomass_maz_po_GA1 ,
+           df_biomass_maz_po_GA2 ,
+           df_biomass_ramian_GA1 ,
+           df_biomass_ramian_GA2 
+)
+
+
+nw_df_biomass <- lapply(df.bio, change_names)  
+
+combined_df_biomass_nw <- 
+  bind_rows(nw_df_biomass, .id = "column_label") %>% 
+  select(sort(tidyselect::peek_vars()))%>%
+  select(-column_label)
+
+write.csv(combined_df_biomass_nw, file ="biomass_correct.csv" )
