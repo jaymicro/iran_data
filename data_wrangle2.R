33e5b0fa93e003e5b01ebacdf062c7ac0e03c5b8
jaymicro <jay.envs@gmail.com>
2021-02-23 12:00:37 -0800

    edited

--- data_wrangle2.R
+++ data_wrangle2.R
@@ -18,21 +18,6 @@ GM_GA2 <- read_excel("GM_GA2.xlsx") %>%
 
 
 
-all_data <- bind_rows(GM_E1,GM_E2,GM_GA1,GM_GA2)
-
-
-
-GM_E1_col_name <- str_extract(colnames(GM_E1), "^[a-z]+_[a-z]+")
-GM_E2_col_name <- str_extract(colnames(GM_E2), "^[a-z]+_[a-z]+")
-GM_GA1_col_name <- str_extract(colnames(GM_GA1), "^[a-z]+_[a-z]+")
-GM_GA2_col_name <- str_extract(colnames(GM_GA2), "^[a-z]+_[a-z]+")
-
-
-colnames(GM_E1) = GM_E1_col_name
-colnames(GM_E2) = GM_E2_col_name
-colnames(GM_GA1) = GM_GA1_col_name
-colnames(GM_GA2) = GM_GA2_col_name
-
 df_extracted_names <- bind_rows(GM_E1,GM_E2,GM_GA1,GM_GA2)
 
 df_percentcover <- read_excel("Iran-Gillan-Masouleh.xlsx", sheet = "Exclosure area1", cell_rows(10:74)) %>% 
